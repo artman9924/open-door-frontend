03ea8d33eee088cbe68e9c6602cbac28fdad937d
Arthur Quinones <arthurq@gmail.com>
2025-05-19 15:11:02 -0400

    Changed font color of Favorites button

--- app.js
+++ app.js
@@ -166,6 +166,7 @@ function goToRead() {
       filterToggle.style.background = "#f8f8f8";
       filterToggle.style.cursor = "pointer";
       filterToggle.style.fontSize = "0.9em";
+      filterToggle.style.color = "666";
 
       filterToggle.addEventListener("click", () => {
         showingFavorites = !showingFavorites;
